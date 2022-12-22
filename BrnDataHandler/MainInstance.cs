ea4550d7665c969b6d159076285eedaade7c8193
JeBobs <jebcraftserver@gmail.com>
2022-12-21 20:41:43 -0500

    start major restructure, add basic command system
    Added base abstract class `Command`, moved entry point code into `MainInstance`, which gets created and stored along with a `DataHandler` (basic abstract data handling) class in the main class, also now renamed from `Program` to `Brn`.

---
+++ BrnDataHandler/MainInstance.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrnDataHandler
+{
+    internal class MainInstance
+    {
+        // Switches
+        public bool convertExtensions = false;
+        public static bool keepExtensions = true;
+        public static bool sortByType = false;
+        public bool debug = false;
+
+        public List<string> Directories = new();
+        public List<string> Files = new();
+
+        //delegate void Command();
+
+        public MainInstance(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                List<string> tokens = new List<string>();
+
+                Command command = new Commands.Command_Null();
+
+                foreach (var arg in args)
+                {
+                    Console.WriteLine($"Launched with argument {arg}");
+
+                    if (Directory.Exists(arg))
+                        Directories.Add(arg);
+
+                    else if (File.Exists(arg))
+                        Files.Add(arg);
+
+                    else
+                        tokens.Add(arg.ToLower());
+
+                    // Really hacky system to do quick debugging.
+                    // Gotta replace with a proper token parsing system.
+                    foreach (string tk in tokens)
+                    {
+                        if (tk.StartsWith("--"))
+                        {
+                            switch (tk) // Switches
+                            {
+                                case "--sort-by-type":
+                                    sortByType = true;
+                                    break;
+                                case "--convert-extensions":
+                                    convertExtensions = true;
+                                    keepExtensions = false;
+                                    break;
+                                case "--keep-original-extensions":
+                                    keepExtensions = true;
+                                    break;
+                                case "--debug":
+                                    debug = true;
+                                    break;
+                            }
+                        }
+                        else
+                        {
+                            switch (tk) // Commands
+                            {
+                                case "recover":
+                                    // Process
+                                    command = new Commands.Command_Recover();
+                                    break;
+                                case "convert-asset-endian":
+                                    // Convert endian
+                                    break;
+                            }
+                        }
+                    }
+
+                    if (convertExtensions)
+                        keepExtensions = false;
+                }
+
+                // Run delegate command
+                command.Run();
+            }
+        }
+    }
+}
